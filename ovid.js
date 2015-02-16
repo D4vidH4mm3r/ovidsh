0f70802ac5c8c57646718e36c81af20d2c1edd00
David Hammer <d4vid.h4mm3r@gmail.com>
2015-02-16 08:32:08 +0100

    Giver referencer til remove-funktionen, så tingene behøver ikke id

--- ovid.js
+++ ovid.js
@@ -2,11 +2,11 @@ function extractText(e) {
 	return e.textContent;
 }
 
-function removeTable() {
-	var theTable   = document.getElementById("search-history-clean");
-	var theOverlay = document.getElementById("search-history-overlay");
-	document.body.removeChild(theTable);
-	document.body.removeChild(theOverlay);
+function makeRemover(table, overlay) {
+	return function() {
+		document.body.removeChild(table);
+		document.body.removeChild(overlay);
+	}
 }
 
 function makeTable() {
@@ -22,21 +22,22 @@ function makeTable() {
 	return out;
 }
 
-var removeButton       = "<button onclick='removeTable()'>Remove this table</button><br><br>"
+var removeButton = "<button onclick='resRemove()'>Remove this table</button><br><br>"
 
-var overlay            = document.createElement("div");
-document.body.appendChild(overlay);
-overlay.onclick        = removeTable;
-overlay.id             = "search-history-overlay";
-overlay.style.cssText  = "background-color: black;" + "opacity: 0.7;" +
+var resOverlay = document.createElement("div");
+resOverlay.style.cssText = "background-color: black;" + "opacity: 0.7;" +
 "z-index: 1000;" + "position: fixed;" + "top: 0;" + "left: 0;" +
 "width: 100%;" + "height: 100%;"
 
 var resTable           = document.createElement("div");
-document.body.appendChild(resTable);
 resTable.id            = "search-history-clean"
 resTable.innerHTML     = removeButton + "<div><pre>" + makeTable() + "<br/><br/>" + "</pre></div>";
 resTable.style.cssText = "background-color: white;" +
 "z-index: 1001;" + "position: fixed;" + "float: left;" + "top: 40px;" +
 "width: 956px;" + "padding: 10px;" +
 "border: 2px solid black;"
+
+var resRemove = makeRemover(resTable, resOverlay);
+resOverlay.onclick = resRemove;
+document.body.appendChild(resOverlay);
+document.body.appendChild(resTable);
