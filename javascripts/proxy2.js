8dea47a83ab3aa762ca3278b1991e931dd7733e3
David Hammer <d4vid.h4mm3r@gmail.com>
2016-08-16 10:59:58 +0200

    Smartere ved https

--- javascripts/proxy2.js
+++ javascripts/proxy2.js
@@ -1,1 +1,8 @@
-window.location.href = window.location.protocol + "//" +window.location.host + ".proxy2-bib.sdu.dk:2048" + window.location.pathname
+var res = window.location.protocol + "//";
+if (window.location.protocol == "https:") {
+    res += window.location.host.replace(/\./g, "-") + ".proxy2-bib.sdu.dk";
+} else {
+    res += window.location.host + ".proxy2-bib.sdu.dk:2048";
+}
+res += window.location.pathname;
+window.location.href = res;
