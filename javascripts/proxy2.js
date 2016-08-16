1b38a7add53b3a2983818978bb403bb62cd172a7
David Hammer <d4vid.h4mm3r@gmail.com>
2016-08-16 10:47:31 +0200

    Redirection

--- javascripts/proxy2.js
+++ javascripts/proxy2.js
@@ -1,1 +1,1 @@
-window.location.protocol + "//" + window.location.host + ".proxy2-bib.sdu.dk:2048" + window.location.pathname
+window.location.href = window.location.protocol + "//" +window.location.host + ".proxy2-bib.sdu.dk:2048" + window.location.pathname
